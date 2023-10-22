186e8236ef6b7b0607629fa2107b8bec7822011a
Dmitry <samoulovdima@mail.ru>
2023-10-23 03:04:48 +0500

    add validate

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-const nav = document.querySelector('#nav')
-const navBtn = document.querySelector('#nav-btn')
-const navBtnImg = document.querySelector('#nav-btn-img')
+const nav = document.querySelector('#nav');
+const navBtn = document.querySelector('#nav-btn');
+const navBtnImg = document.querySelector('#nav-btn-img');
 
 navBtn.addEventListener ('click', () => {
     if (nav.classList.toggle('open')) {
@@ -8,13 +8,13 @@ navBtn.addEventListener ('click', () => {
     } else {
         navBtnImg.src = "./img/icons/nav-open.svg"
     }
-})
+});
 
 AOS.init({
     once: true
 });
 
-const modal = document.createElement('div')
+const modal = document.createElement('div');
 
 modal.insertAdjacentHTML (
     "afterbegin",
@@ -22,19 +22,19 @@ modal.insertAdjacentHTML (
         <form class="popup">
             <img src="img/icons/nav-close.svg" alt="close" class="close-popup">
             <label>
-                <input type="text" class="last-name">
+                <input type="text" class="last-name _req">
                 <div class="label__text">Last name</div>
             </label>
             <label>
-                <input type="text" class="first-name">
+                <input type="text" class="first-name _req">
                 <div class="label__text">First name</div>
             </label>
             <label>
-                <input type="text" class="middle-name">
+                <input type="text" class="middle-name _req">
                 <div class="label__text">Middle name</div>
             </label>
             <label>
-                <input type="text" class="email">
+                <input type="text" class="email _req _email">
                 <div class="label__text">Email</div>
             </label>
             <label>
@@ -44,30 +44,73 @@ modal.insertAdjacentHTML (
             <button type="submit">Send</button>
         </form>
     </div>`
- )
+ );
 
-document.body.appendChild(modal)
+document.body.appendChild(modal);
 
-const popup__bg = document.querySelector('.popup__bg')
-const popup = document.querySelector('.popup')
-const openPopup = document.querySelector('.send-message-btn')
-const closePopup = document.querySelector('.close-popup')
+const popup__bg = document.querySelector('.popup__bg');
+const popup = document.querySelector('.popup');
+const openPopup = document.querySelector('.send-message-btn');
+const closePopup = document.querySelector('.close-popup');
 
 openPopup.addEventListener ('click', (e) => {
-    e.preventDefault
-    popup__bg.classList.add('active')
-    popup.classList.add('active')
+    e.preventDefault;
+    popup__bg.classList.add('active');
+    popup.classList.add('active');
 })
 
-closePopup.addEventListener ('click', funClosePopup)
+closePopup.addEventListener ('click', funClosePopup);
 
 function funClosePopup () {
-    popup__bg.classList.remove('active')
-    popup.classList.remove('active')
+    popup__bg.classList.remove('active');
+    popup.classList.remove('active');
 }
 
-document.addEventListener ('click', (e) => {
-    if (e.target === popup__bg) {
-        funClosePopup()
+// document.addEventListener ('click', (e) => {
+//     if (e.target === popup__bg) {
+//         funClosePopup();
+//     }
+// })
+
+popup.addEventListener('submit', popupSend);
+
+async function popupSend(e) {
+    e.preventDefault();
+    let error = popupValidate(popup);
+    
+}
+
+function popupValidate(popup) {
+    let error = 0;
+    let popupReq = document.querySelectorAll('._req');
+
+    for (let index = 0; index < popupReq.length; index++) {
+        const input = popupReq[index];
+        popupRemoveError(input);
+
+        if (input.classList.contains('_email')){
+            if (emailTest(input)) {
+                popupAddError(input);
+                error++;
+            }
+        }else if(input.value === '') {
+            popupAddError(input);
+            error++;
+        }
     }
-})
+    return error;
+}
+
+function popupAddError(input) {
+    input.parentElement.classList.add('_error');
+    input.classList.add('_error');
+}
+
+function popupRemoveError(input) {
+    input.parentElement.classList.remove('_error');
+    input.classList.remove('_error');
+}
+
+function emailTest(input) {
+    return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+}
