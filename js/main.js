a629e36e139495ec16bd8f387da49f49456080f7
Dmitry <samoulovdima@mail.ru>
2023-10-23 01:06:34 +0500

    add popup

--- js/main.js
+++ js/main.js
@@ -2,17 +2,72 @@ const nav = document.querySelector('#nav')
 const navBtn = document.querySelector('#nav-btn')
 const navBtnImg = document.querySelector('#nav-btn-img')
 
-
-navBtn.addEventListener ('click', funForNavBtn)
-
-function funForNavBtn () {
+navBtn.addEventListener ('click', () => {
     if (nav.classList.toggle('open')) {
         navBtnImg.src = "./img/icons/nav-close.svg"
     } else {
         navBtnImg.src = "./img/icons/nav-open.svg"
     }
-}
+})
 
 AOS.init({
     once: true
 });
+
+const modal = document.createElement('div')
+
+modal.insertAdjacentHTML (
+    "afterbegin",
+    `<div class="popup__bg">
+        <form class="popup">
+            <img src="img/icons/nav-close.svg" alt="close" class="close-popup">
+            <label>
+                <input type="text" class="last-name">
+                <div class="label__text">Last name</div>
+            </label>
+            <label>
+                <input type="text" class="first-name">
+                <div class="label__text">First name</div>
+            </label>
+            <label>
+                <input type="text" class="middle-name">
+                <div class="label__text">Middle name</div>
+            </label>
+            <label>
+                <input type="text" class="email">
+                <div class="label__text">Email</div>
+            </label>
+            <label>
+                <textarea name="message"></textarea>
+                <div class="label__text">Your message</div>
+            </label>
+            <button type="submit">Send</button>
+        </form>
+    </div>`
+ )
+
+document.body.appendChild(modal)
+
+const popup__bg = document.querySelector('.popup__bg')
+const popup = document.querySelector('.popup')
+const openPopup = document.querySelector('.send-message-btn')
+const closePopup = document.querySelector('.close-popup')
+
+openPopup.addEventListener ('click', (e) => {
+    e.preventDefault
+    popup__bg.classList.add('active')
+    popup.classList.add('active')
+})
+
+closePopup.addEventListener ('click', funClosePopup)
+
+function funClosePopup () {
+    popup__bg.classList.remove('active')
+    popup.classList.remove('active')
+}
+
+document.addEventListener ('click', (e) => {
+    if (e.target === popup__bg) {
+        funClosePopup()
+    }
+})
